63e7d188182f53e936ba95d626eddef321aa6f17
RubyWaves <karol.przy419@gmail.com>
2023-08-30 17:02:23 +0200

    added units for temperature and wind speed

--- src/js/elements/WeatherRow.jsx
+++ src/js/elements/WeatherRow.jsx
@@ -2,8 +2,8 @@ export default function WeatherRow({ name, temperature, windSpeed }) {
 	return (
 		<div className="tab-row weather-row">
 			<span className="weather-row__name">{name} </span>
-			<span className="weather-row__temperature">{temperature} </span>
-			<span className="weather-row__wind-speed">{windSpeed} </span>
+			<span className="weather-row__temperature">{temperature} °C</span>
+			<span className="weather-row__wind-speed">{windSpeed} km/h</span>
 		</div>
 	);
 }
