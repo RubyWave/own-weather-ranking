707659cfd91f3fc2ca714e5f5c0c518a800ec374
RubyWaves <karol.przy419@gmail.com>
2023-09-01 16:19:13 +0200

    added button to remove row of weather in the table

--- src/js/elements/WeatherRow.jsx
+++ src/js/elements/WeatherRow.jsx
@@ -1,7 +1,25 @@
+import closeIcon from "../../../public/images/close-icon.svg";
+import { useWeathersListDispatcher } from "../contexts/WeathersList";
+
 export default function WeatherRow({ name, temperature, windSpeed }) {
+	const dispatch = useWeathersListDispatcher();
+
+	function handleClick(e) {
+		dispatch({
+			type: "remove",
+			name: name,
+		});
+	}
 	return (
 		<div className="tab-row weather-row">
-			<span className="weather-row__name">{name} </span>
+			<div className="weather-row__name">
+				<span>{name}</span>
+				<img
+					className="weather-row__remover"
+					onClick={handleClick}
+					src={closeIcon}
+				/>{" "}
+			</div>
 			<span className="weather-row__temperature">{temperature} °C</span>
 			<span className="weather-row__wind-speed">{windSpeed} km/h</span>
 		</div>
